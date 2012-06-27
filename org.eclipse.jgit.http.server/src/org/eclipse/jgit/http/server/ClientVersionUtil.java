eclipse-jgit/jgit

Work around smart HTTP bugs in C Git
I have unfortunately introduced a few bugs in the native Git client
over the years. 1.7.5 is unable to send chunked requests correctly,
resulting in corrupt data at the server. Ban this client whenever
it uses chunked encoding with an error message.

Prior to some more recent versions, git push over HTTP failed to
report status information and error messages due to a race within
the client and its helper process. Check for these bad versions and
send errors as messages before the status report, enabling users
to see the failures on their terminal.

Change-Id: Ic62d6591cbd851d21dbb3e9b023d655eaecb0624

org.eclipse.jgit.http.server/src/org/eclipse/jgit/http/server/ClientVersionUtil.java
@@ -1,0 +1,201 @@
+/*
+ * Copyright (C) 2012, Google Inc.
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.http.server;
+
+import static org.eclipse.jgit.util.HttpSupport.HDR_CONTENT_ENCODING;
+import static org.eclipse.jgit.util.HttpSupport.HDR_USER_AGENT;
+
+import javax.servlet.http.HttpServletRequest;
+
+/** Parses Git client User-Agent strings. */
+public class ClientVersionUtil {
+	private static final int[] v1_7_5 = { 1, 7, 5 };
+	private static final int[] v1_7_8_6 = { 1, 7, 8, 6 };
+	private static final int[] v1_7_9 = { 1, 7, 9 };
+
+	/** @return maximum version array, indicating an invalid version of Git. */
+	public static int[] invalidVersion() {
+		return new int[] { Integer.MAX_VALUE };
+	}
+
+	/**
+	 * Parse a Git client User-Agent header value.
+	 *
+	 * @param version
+	 *            git client version string, of the form "git/1.7.9".
+	 * @return components of the version string. {@link #invalidVersion()} if
+	 *         the version string cannot be parsed.
+	 */
+	public static int[] parse(String version) {
+		if (version != null && version.startsWith("git/"))
+			return splitVersion(version.substring("git/".length()));
+		return invalidVersion();
+	}
+
+	private static int[] splitVersion(String versionString) {
+		char[] str = versionString.toCharArray();
+		int[] ver = new int[4];
+		int end = 0;
+		int acc = 0;
+		for (int i = 0; i < str.length; i++) {
+			char c = str[i];
+			if ('0' <= c && c <= '9') {
+				acc *= 10;
+				acc += c - '0';
+			} else if (c == '.') {
+				if (end == ver.length)
+					ver = grow(ver);
+				ver[end++] = acc;
+				acc = 0;
+			} else if (c == 'g' && 0 < i && str[i - 1] == '.' && 0 < end) {
+				// Non-tagged builds may contain a mangled git describe output.
+				// "1.7.6.1.45.gbe0cc". The 45 isn't a valid component. Drop it.
+				ver[end - 1] = 0;
+				acc = 0;
+				break;
+			} else if (c == '-' && (i + 2) < str.length
+					&& str[i + 1] == 'r' && str[i + 2] == 'c') {
+				// Release candidates aren't the same as a final release.
+				acc = 0;
+				break;
+			} else
+				break;
+		}
+		if (acc != 0) {
+			if (end == ver.length)
+				ver = grow(ver);
+			ver[end++] = acc;
+		} else {
+			while (0 < end && ver[end - 1] == 0)
+				end--;
+		}
+		if (end < ver.length) {
+			int[] n = new int[end];
+			System.arraycopy(ver, 0, n, 0, end);
+			ver = n;
+		}
+		return ver;
+	}
+
+	private static int[] grow(int[] tmp) {
+		int[] n = new int[tmp.length + 1];
+		System.arraycopy(tmp, 0, n, 0, tmp.length);
+		return n;
+	}
+
+	/**
+	 * Compare two version strings for natural ordering.
+	 *
+	 * @param a
+	 *            first parsed version string.
+	 * @param b
+	 *            second parsed version string.
+	 * @return <0 if a is before b; 0 if a equals b; >0 if a is after b.
+	 */
+	public static int compare(int[] a, int[] b) {
+		for (int i = 0; i < a.length && i < b.length; i++) {
+			int cmp = a[i] - b[i];
+			if (cmp != 0)
+				return cmp;
+		}
+		return a.length - b.length;
+	}
+
+	/**
+	 * Convert a parsed version back to a string.
+	 *
+	 * @param ver
+	 *            the parsed version array.
+	 * @return a string, e.g. "1.6.6.0".
+	 */
+	public static String toString(int[] ver) {
+		StringBuilder b = new StringBuilder();
+		for (int v : ver) {
+			if (b.length() > 0)
+				b.append('.');
+			b.append(v);
+		}
+		return b.toString();
+	}
+
+	/**
+	 * Check if a Git client has the known push status bug.
+	 * <p>
+	 * These buggy clients do not display the status report from a failed push
+	 * over HTTP.
+	 *
+	 * @param ver
+	 *            parsed version of the client.
+	 * @return true if the bug is present.
+	 */
+	public static boolean hasPushStatusBug(int[] ver) {
+		int cmp = compare(ver, v1_7_8_6);
+		if (cmp < 0)
+			return true; // Everything before 1.7.8.6 is known broken.
+		else if (cmp == 0)
+			return false; // 1.7.8.6 contained the bug fix.
+
+		if (compare(ver, v1_7_9) <= 0)
+			return true; // 1.7.9 shipped before 1.7.8.6 and has the bug.
+		return false; // 1.7.9.1 and later are fixed.
+	}
+
+	/**
+	 * Check if a Git client has the known chunked request body encoding bug.
+	 * <p>
+	 * Git 1.7.5 contains a unique bug where chunked requests are malformed.
+	 * This applies to both fetch and push.
+	 *
+	 * @param req
+	 *            incoming HTTP request.
+	 * @return true if the client has the chunked encoding bug.
+	 */
+	public static boolean hasChunkedRequestBug(HttpServletRequest req) {
+		return "chunked".equalsIgnoreCase(req.getHeader(HDR_CONTENT_ENCODING))
+				&& compare(parse(req.getHeader(HDR_USER_AGENT)), v1_7_5) == 0;
+	}
+
+	private ClientVersionUtil() {
+	}
+}

org.eclipse.jgit.http.test/tst/org/eclipse/jgit/http/server/ClientVersionUtilTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010, Sasa Zivkov <sasa.zivkov@sap.com>
+ * Copyright (C) 2012, Google Inc.
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -43,42 +43,43 @@
 
 package org.eclipse.jgit.http.server;
 
-import org.eclipse.jgit.nls.NLS;
-import org.eclipse.jgit.nls.TranslationBundle;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.eclipse.jgit.http.server.ClientVersionUtil.hasPushStatusBug;
+import static org.eclipse.jgit.http.server.ClientVersionUtil.invalidVersion;
+import static org.eclipse.jgit.http.server.ClientVersionUtil.parse;
 
-/**
- * Translation bundle for JGit http server
- */
-public class HttpServerText extends TranslationBundle {
+import org.junit.Assert;
+import org.junit.Test;
 
-	/**
-	 * @return an instance of this translation bundle
-	 */
-	public static HttpServerText get() {
-		return NLS.getBundleFor(HttpServerText.class);
+public class ClientVersionUtilTest {
+	@Test
+	public void testParse() {
+		assertEquals("1.6", parse("git/1.6.6-rc0"));
+		assertEquals("1.6.6", parse("git/1.6.6"));
+		assertEquals("1.7.5", parse("git/1.7.5.GIT"));
+		assertEquals("1.7.6.1", parse("git/1.7.6.1.45.gbe0cc"));
+
+		assertEquals("1.5.4.3", parse("git/1.5.4.3,gzip(proxy)"));
+		assertEquals("1.7.0.2", parse("git/1.7.0.2.msysgit.0.14.g956d7,gzip"));
+		assertEquals("1.7.10.2", parse("git/1.7.10.2 (Apple Git-33)"));
+
+		assertEquals(ClientVersionUtil.toString(invalidVersion()), parse("foo"));
 	}
 
-	/***/ public String alreadyInitializedByContainer;
-	/***/ public String cannotGetLengthOf;
-	/***/ public String clientHas175ChunkedEncodingBug;
-	/***/ public String encodingNotSupportedByThisLibrary;
-	/***/ public String expectedRepositoryAttribute;
-	/***/ public String filterMustNotBeNull;
-	/***/ public String internalErrorDuringReceivePack;
-	/***/ public String internalErrorDuringUploadPack;
-	/***/ public String internalServerError;
-	/***/ public String internalServerErrorRequestAttributeWasAlreadySet;
-	/***/ public String invalidBoolean;
-	/***/ public String invalidIndex;
-	/***/ public String invalidRegexGroup;
-	/***/ public String noResolverAvailable;
-	/***/ public String parameterNotSet;
-	/***/ public String pathForParamNotFound;
-	/***/ public String pathNotSupported;
-	/***/ public String repositoryAccessForbidden;
-	/***/ public String repositoryNotFound;
-	/***/ public String servletAlreadyInitialized;
-	/***/ public String servletMustNotBeNull;
-	/***/ public String servletWasAlreadyBound;
-	/***/ public String unexpectedeOFOn;
+	@Test
+	public void testPushStatusBug() {
+		assertTrue(hasPushStatusBug(parse("git/1.6.6")));
+		assertTrue(hasPushStatusBug(parse("git/1.6.6.1")));
+		assertTrue(hasPushStatusBug(parse("git/1.7.9")));
+
+		assertFalse(hasPushStatusBug(parse("git/1.7.8.6")));
+		assertFalse(hasPushStatusBug(parse("git/1.7.9.1")));
+		assertFalse(hasPushStatusBug(parse("git/1.7.9.2")));
+		assertFalse(hasPushStatusBug(parse("git/1.7.10")));
+	}
+
+	private static void assertEquals(String exp, int[] act) {
+		Assert.assertEquals(exp, ClientVersionUtil.toString(act));
+	}
 }

ShoOgino/jgitFile

org.eclipse.jgit.http.server/src/org/eclipse/jgit/http/server/ClientVersionUtil.java
@@ -1,0 +1,203 @@
+/*
+ * Copyright (C) 2012, Google Inc.
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.http.server;
+
+import static org.eclipse.jgit.http.server.ServletUtils.isChunked;
+
+import javax.servlet.http.HttpServletRequest;
+
+/** Parses Git client User-Agent strings. */
+public class ClientVersionUtil {
+	private static final int[] v1_7_5 = { 1, 7, 5 };
+	private static final int[] v1_7_8_6 = { 1, 7, 8, 6 };
+	private static final int[] v1_7_9 = { 1, 7, 9 };
+
+	/** @return maximum version array, indicating an invalid version of Git. */
+	public static int[] invalidVersion() {
+		return new int[] { Integer.MAX_VALUE };
+	}
+
+	/**
+	 * Parse a Git client User-Agent header value.
+	 *
+	 * @param version
+	 *            git client version string, of the form "git/1.7.9".
+	 * @return components of the version string. {@link #invalidVersion()} if
+	 *         the version string cannot be parsed.
+	 */
+	public static int[] parseVersion(String version) {
+		if (version != null && version.startsWith("git/"))
+			return splitVersion(version.substring("git/".length()));
+		return invalidVersion();
+	}
+
+	private static int[] splitVersion(String versionString) {
+		char[] str = versionString.toCharArray();
+		int[] ver = new int[4];
+		int end = 0;
+		int acc = 0;
+		for (int i = 0; i < str.length; i++) {
+			char c = str[i];
+			if ('0' <= c && c <= '9') {
+				acc *= 10;
+				acc += c - '0';
+			} else if (c == '.') {
+				if (end == ver.length)
+					ver = grow(ver);
+				ver[end++] = acc;
+				acc = 0;
+			} else if (c == 'g' && 0 < i && str[i - 1] == '.' && 0 < end) {
+				// Non-tagged builds may contain a mangled git describe output.
+				// "1.7.6.1.45.gbe0cc". The 45 isn't a valid component. Drop it.
+				ver[end - 1] = 0;
+				acc = 0;
+				break;
+			} else if (c == '-' && (i + 2) < str.length
+					&& str[i + 1] == 'r' && str[i + 2] == 'c') {
+				// Release candidates aren't the same as a final release.
+				if (acc > 0)
+					acc--;
+				break;
+			} else
+				break;
+		}
+		if (acc != 0) {
+			if (end == ver.length)
+				ver = grow(ver);
+			ver[end++] = acc;
+		} else {
+			while (0 < end && ver[end - 1] == 0)
+				end--;
+		}
+		if (end < ver.length) {
+			int[] n = new int[end];
+			System.arraycopy(ver, 0, n, 0, end);
+			ver = n;
+		}
+		return ver;
+	}
+
+	private static int[] grow(int[] tmp) {
+		int[] n = new int[tmp.length + 1];
+		System.arraycopy(tmp, 0, n, 0, tmp.length);
+		return n;
+	}
+
+	/**
+	 * Compare two version strings for natural ordering.
+	 *
+	 * @param a
+	 *            first parsed version string.
+	 * @param b
+	 *            second parsed version string.
+	 * @return <0 if a is before b; 0 if a equals b; >0 if a is after b.
+	 */
+	public static int compare(int[] a, int[] b) {
+		for (int i = 0; i < a.length && i < b.length; i++) {
+			int cmp = a[i] - b[i];
+			if (cmp != 0)
+				return cmp;
+		}
+		return a.length - b.length;
+	}
+
+	/**
+	 * Convert a parsed version back to a string.
+	 *
+	 * @param ver
+	 *            the parsed version array.
+	 * @return a string, e.g. "1.6.6.0".
+	 */
+	public static String toString(int[] ver) {
+		StringBuilder b = new StringBuilder();
+		for (int v : ver) {
+			if (b.length() > 0)
+				b.append('.');
+			b.append(v);
+		}
+		return b.toString();
+	}
+
+	/**
+	 * Check if a Git client has the known push status bug.
+	 * <p>
+	 * These buggy clients do not display the status report from a failed push
+	 * over HTTP.
+	 *
+	 * @param version
+	 *            parsed version of the Git client software.
+	 * @return true if the bug is present.
+	 */
+	public static boolean hasPushStatusBug(int[] version) {
+		int cmp = compare(version, v1_7_8_6);
+		if (cmp < 0)
+			return true; // Everything before 1.7.8.6 is known broken.
+		else if (cmp == 0)
+			return false; // 1.7.8.6 contained the bug fix.
+
+		if (compare(version, v1_7_9) <= 0)
+			return true; // 1.7.9 shipped before 1.7.8.6 and has the bug.
+		return false; // 1.7.9.1 and later are fixed.
+	}
+
+	/**
+	 * Check if a Git client has the known chunked request body encoding bug.
+	 * <p>
+	 * Git 1.7.5 contains a unique bug where chunked requests are malformed.
+	 * This applies to both fetch and push.
+	 *
+	 * @param version
+	 *            parsed version of the Git client software.
+	 * @param request
+	 *            incoming HTTP request.
+	 * @return true if the client has the chunked encoding bug.
+	 */
+	public static boolean hasChunkedEncodingRequestBug(
+			int[] version, HttpServletRequest request) {
+		return compare(version, v1_7_5) == 0 && isChunked(request);
+	}
+
+	private ClientVersionUtil() {
+	}
+}

org.eclipse.jgit.http.test/tst/org/eclipse/jgit/http/server/ClientVersionUtilTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010, Sasa Zivkov <sasa.zivkov@sap.com>
+ * Copyright (C) 2012, Google Inc.
  * and other copyright owners as documented in the project's IP log.
  *
  * This program and the accompanying materials are made available
@@ -43,42 +43,43 @@
 
 package org.eclipse.jgit.http.server;
 
-import org.eclipse.jgit.nls.NLS;
-import org.eclipse.jgit.nls.TranslationBundle;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.eclipse.jgit.http.server.ClientVersionUtil.hasPushStatusBug;
+import static org.eclipse.jgit.http.server.ClientVersionUtil.invalidVersion;
+import static org.eclipse.jgit.http.server.ClientVersionUtil.parseVersion;
 
-/**
- * Translation bundle for JGit http server
- */
-public class HttpServerText extends TranslationBundle {
+import org.junit.Assert;
+import org.junit.Test;
 
-	/**
-	 * @return an instance of this translation bundle
-	 */
-	public static HttpServerText get() {
-		return NLS.getBundleFor(HttpServerText.class);
+public class ClientVersionUtilTest {
+	@Test
+	public void testParse() {
+		assertEquals("1.6.5", parseVersion("git/1.6.6-rc0"));
+		assertEquals("1.6.6", parseVersion("git/1.6.6"));
+		assertEquals("1.7.5", parseVersion("git/1.7.5.GIT"));
+		assertEquals("1.7.6.1", parseVersion("git/1.7.6.1.45.gbe0cc"));
+
+		assertEquals("1.5.4.3", parseVersion("git/1.5.4.3,gzip(proxy)"));
+		assertEquals("1.7.0.2", parseVersion("git/1.7.0.2.msysgit.0.14.g956d7,gzip"));
+		assertEquals("1.7.10.2", parseVersion("git/1.7.10.2 (Apple Git-33)"));
+
+		assertEquals(ClientVersionUtil.toString(invalidVersion()), parseVersion("foo"));
 	}
 
-	/***/ public String alreadyInitializedByContainer;
-	/***/ public String cannotGetLengthOf;
-	/***/ public String clientHas175ChunkedEncodingBug;
-	/***/ public String encodingNotSupportedByThisLibrary;
-	/***/ public String expectedRepositoryAttribute;
-	/***/ public String filterMustNotBeNull;
-	/***/ public String internalErrorDuringReceivePack;
-	/***/ public String internalErrorDuringUploadPack;
-	/***/ public String internalServerError;
-	/***/ public String internalServerErrorRequestAttributeWasAlreadySet;
-	/***/ public String invalidBoolean;
-	/***/ public String invalidIndex;
-	/***/ public String invalidRegexGroup;
-	/***/ public String noResolverAvailable;
-	/***/ public String parameterNotSet;
-	/***/ public String pathForParamNotFound;
-	/***/ public String pathNotSupported;
-	/***/ public String repositoryAccessForbidden;
-	/***/ public String repositoryNotFound;
-	/***/ public String servletAlreadyInitialized;
-	/***/ public String servletMustNotBeNull;
-	/***/ public String servletWasAlreadyBound;
-	/***/ public String unexpectedeOFOn;
+	@Test
+	public void testPushStatusBug() {
+		assertTrue(hasPushStatusBug(parseVersion("git/1.6.6")));
+		assertTrue(hasPushStatusBug(parseVersion("git/1.6.6.1")));
+		assertTrue(hasPushStatusBug(parseVersion("git/1.7.9")));
+
+		assertFalse(hasPushStatusBug(parseVersion("git/1.7.8.6")));
+		assertFalse(hasPushStatusBug(parseVersion("git/1.7.9.1")));
+		assertFalse(hasPushStatusBug(parseVersion("git/1.7.9.2")));
+		assertFalse(hasPushStatusBug(parseVersion("git/1.7.10")));
+	}
+
+	private static void assertEquals(String exp, int[] act) {
+		Assert.assertEquals(exp, ClientVersionUtil.toString(act));
+	}
 }