eclipse-jgit/jgit

Rename local variables/parameters to remove warnings about hiding
Change-Id: I73f38492b6a2e7fd6e77005efd0a8a8c65763e74

org.eclipse.jgit/src/org/eclipse/jgit/merge/RecursiveMerger.java
@@ -254,17 +254,17 @@ public class RecursiveMerger extends ResolveMerger {
 	 */
 	private DirCache dircacheFromTree(ObjectId treeId) throws IOException {
 		DirCache ret = DirCache.newInCore();
-		DirCacheBuilder builder = ret.builder();
-		TreeWalk tw = new TreeWalk(db);
-		tw.addTree(treeId);
-		tw.setRecursive(true);
-		while (tw.next()) {
-			DirCacheEntry e = new DirCacheEntry(tw.getRawPath());
-			e.setFileMode(tw.getFileMode(0));
-			e.setObjectId(tw.getObjectId(0));
-			builder.add(e);
+		DirCacheBuilder aBuilder = ret.builder();
+		TreeWalk atw = new TreeWalk(db);
+		atw.addTree(treeId);
+		atw.setRecursive(true);
+		while (atw.next()) {
+			DirCacheEntry e = new DirCacheEntry(atw.getRawPath());
+			e.setFileMode(atw.getFileMode(0));
+			e.setObjectId(atw.getObjectId(0));
+			aBuilder.add(e);
 		}
-		builder.finish();
+		aBuilder.finish();
 		return ret;
 	}
 }

ShoOgino/jgitFile

org.eclipse.jgit/src/org/eclipse/jgit/merge/RecursiveMerger.java
@@ -264,17 +264,17 @@ public class RecursiveMerger extends ResolveMerger {
 	 */
 	private DirCache dircacheFromTree(ObjectId treeId) throws IOException {
 		DirCache ret = DirCache.newInCore();
-		DirCacheBuilder builder = ret.builder();
-		TreeWalk tw = new TreeWalk(reader);
-		tw.addTree(treeId);
-		tw.setRecursive(true);
-		while (tw.next()) {
-			DirCacheEntry e = new DirCacheEntry(tw.getRawPath());
-			e.setFileMode(tw.getFileMode(0));
-			e.setObjectId(tw.getObjectId(0));
-			builder.add(e);
+		DirCacheBuilder aBuilder = ret.builder();
+		TreeWalk atw = new TreeWalk(reader);
+		atw.addTree(treeId);
+		atw.setRecursive(true);
+		while (atw.next()) {
+			DirCacheEntry e = new DirCacheEntry(atw.getRawPath());
+			e.setFileMode(atw.getFileMode(0));
+			e.setObjectId(atw.getObjectId(0));
+			aBuilder.add(e);
 		}
-		builder.finish();
+		aBuilder.finish();
 		return ret;
 	}
 }