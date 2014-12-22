eclipse-jgit/jgit

[pgm] Add option --bare to clone command
Change-Id: I528491e3e20d3c9ebe37fe3cd2bf85b4644d8698
Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

org.eclipse.jgit.pgm.test/tst/org/eclipse/jgit/pgm/CloneTest.java
@@ -43,6 +43,7 @@
 package org.eclipse.jgit.pgm;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.util.List;
@@ -80,5 +81,21 @@ public class CloneTest extends CLIRepositoryTestCase {
 		Git git = Git.open(new File(targetPath));
 		List<Ref> branches = git.branchList().call();
 		assertEquals("expected 1 branch", 1, branches.size());
+	}
+
+	@Test
+	public void testCloneBare() throws Exception {
+		File gitDir = db.getDirectory();
+		String sourcePath = gitDir.getAbsolutePath();
+		String targetPath = (new File(sourcePath)).getParentFile()
+				.getParentFile().getAbsolutePath()
+				+ "/target.git";
+		StringBuilder cmd = new StringBuilder("git clone --bare ")
+				.append(sourcePath + " " + targetPath);
+		execute(cmd.toString());
+		Git git = Git.open(new File(targetPath));
+		List<Ref> branches = git.branchList().call();
+		assertEquals("expected 1 branch", 1, branches.size());
+		assertTrue("expected bare repository", git.getRepository().isBare());
 	}
 }

ShoOgino/jgitFile

org.eclipse.jgit.pgm.test/tst/org/eclipse/jgit/pgm/CloneTest.java
@@ -44,6 +44,7 @@ package org.eclipse.jgit.pgm;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.util.List;
@@ -132,5 +133,24 @@ public class CloneTest extends CLIRepositoryTestCase {
 		Git git2 = Git.open(new File(target, name));
 		List<Ref> branches = git2.branchList().call();
 		assertEquals("expected 1 branch", 1, branches.size());
+	}
+
+	public void testCloneBare() throws Exception {
+		createInitialCommit();
+
+		File gitDir = db.getDirectory();
+		String sourcePath = gitDir.getAbsolutePath();
+		String targetPath = (new File(sourcePath)).getParentFile()
+				.getParentFile().getAbsolutePath()
+				+ "/target.git";
+		StringBuilder cmd = new StringBuilder("git clone --bare ")
+				.append(sourcePath + " " + targetPath);
+		String[] result = execute(cmd.toString());
+		assertArrayEquals(new String[] {
+				"Cloning into '" + targetPath + "'...", "", "" }, result);
+		Git git2 = Git.open(new File(targetPath));
+		List<Ref> branches = git2.branchList().call();
+		assertEquals("expected 1 branch", 1, branches.size());
+		assertTrue("expected bare repository", git2.getRepository().isBare());
 	}
 }