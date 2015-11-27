eclipse-jgit/jgit

Add the new class Attributes holding multiple Attribute(s)
Attributes represents a semantic collector of Attribute(s) and replaces
the anonymous Map<String,Attribute>. This class will be returned by
TreeWalk.getAttributes(). It offers convenient access to the attributes
wrapped in the Attributes object. Adds preparations for a future
Attribute Macro Expansion

Change-Id: I8348c8c457a2a7f1f0c48050e10399b0fa1cdbe1
Signed-off-by: Ivan Motsch <ivan.motsch@bsiag.com>

org.eclipse.jgit.test/tst/org/eclipse/jgit/attributes/AttributesNodeDirCacheIteratorTest.java
@@ -52,9 +52,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.attributes.Attribute.State;
@@ -258,12 +256,14 @@ public class AttributesNodeDirCacheIteratorTest extends RepositoryTestCase {
 			assertTrue(nodeAttrs == null || nodeAttrs.isEmpty());
 		else {
 
-			Map<String, Attribute> entryAttributes = new LinkedHashMap<String, Attribute>();
-			attributesNode.getAttributes(pathName, false, entryAttributes);
+			Attributes entryAttributes = new Attributes();
+			attributesNode.getAttributes(pathName,
+					false, entryAttributes);
 
 			if (nodeAttrs != null && !nodeAttrs.isEmpty()) {
 				for (Attribute attribute : nodeAttrs) {
-					assertThat(entryAttributes.values(), hasItem(attribute));
+					assertThat(entryAttributes.getAttributes(),
+							hasItem(attribute));
 				}
 			} else {
 				assertTrue(

org.eclipse.jgit.test/tst/org/eclipse/jgit/attributes/AttributesNodeWorkingTreeIteratorTest.java
@@ -53,9 +53,7 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.eclipse.jgit.attributes.Attribute.State;
 import org.eclipse.jgit.errors.CorruptObjectException;
@@ -227,12 +225,14 @@ public class AttributesNodeWorkingTreeIteratorTest extends RepositoryTestCase {
 			assertTrue(nodeAttrs == null || nodeAttrs.isEmpty());
 		else {
 
-			Map<String, Attribute> entryAttributes = new LinkedHashMap<String, Attribute>();
-			attributesNode.getAttributes(pathName, false, entryAttributes);
+			Attributes entryAttributes = new Attributes();
+			attributesNode.getAttributes(pathName,
+					false, entryAttributes);
 
 			if (nodeAttrs != null && !nodeAttrs.isEmpty()) {
 				for (Attribute attribute : nodeAttrs) {
-					assertThat(entryAttributes.values(), hasItem(attribute));
+					assertThat(entryAttributes.getAttributes(),
+							hasItem(attribute));
 				}
 			} else {
 				assertTrue(

org.eclipse.jgit.test/tst/org/eclipse/jgit/attributes/TreeWalkAttributeTest.java
@@ -802,8 +802,9 @@ public class TreeWalkAttributeTest extends RepositoryTestCase {
 		assertEquals(type, walk.getFileMode(0));
 
 		assertEquals(checkinAttributes,
-				asSet(ci_walk.getAttributes().values()));
-		assertEquals(checkoutAttributes, asSet(walk.getAttributes().values()));
+				asSet(ci_walk.getAttributes().getAttributes()));
+		assertEquals(checkoutAttributes,
+				asSet(walk.getAttributes().getAttributes()));
 
 		if (D.equals(type)) {
 			walk.enterSubtree();

org.eclipse.jgit/src/org/eclipse/jgit/attributes/Attributes.java
@@ -1,0 +1,184 @@
+/*
+ * Copyright (C) 2015, Ivan Motsch <ivan.motsch@bsiag.com>
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.attributes;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.eclipse.jgit.attributes.Attribute.State;
+
+/**
+ * Represents a set of attributes for a path
+ * <p>
+ *
+ * @since 4.2
+ */
+public final class Attributes {
+	private final Map<String, Attribute> map = new LinkedHashMap<>();
+
+	/**
+	 * Creates a new instance
+	 *
+	 * @param attributes
+	 */
+	public Attributes(Attribute... attributes) {
+		if (attributes != null) {
+			for (Attribute a : attributes) {
+				putAttribute(a);
+			}
+		}
+	}
+
+	/**
+	 * @return true if the set does not contain any attributes
+	 */
+	public boolean isEmpty() {
+		return map.isEmpty();
+	}
+
+	/**
+	 * @param key
+	 * @return the attribute or null
+	 */
+	public Attribute getAttribute(String key) {
+		return map.get(key);
+	}
+
+	/**
+	 * @return all attributes
+	 */
+	public Collection<Attribute> getAttributes() {
+		return new ArrayList<>(map.values());
+	}
+
+	/**
+	 * @param a
+	 */
+	public void putAttribute(Attribute a) {
+		map.put(a.getKey(), a);
+	}
+
+	/**
+	 * @param key
+	 */
+	public void removeAttribute(String key) {
+		map.remove(key);
+	}
+
+	/**
+	 * @param key
+	 * @return true if the {@link Attributes} contains this key
+	 */
+	public boolean containsKey(String key) {
+		return map.containsKey(key);
+	}
+
+	/**
+	 * Returns the state.
+	 *
+	 * @param key
+	 *
+	 * @return the state (never returns <code>null</code>)
+	 */
+	public Attribute.State getState(String key) {
+		Attribute a = map.get(key);
+		return a != null ? a.getState() : Attribute.State.UNSPECIFIED;
+	}
+
+	/**
+	 * @param key
+	 * @return true uf the key is {@link State#SET}, false in all other cases
+	 */
+	public boolean isSet(String key) {
+		return (getState(key) == State.SET);
+	}
+
+	/**
+	 * @param key
+	 * @return true uf the key is {@link State#UNSET}, false in all other cases
+	 */
+	public boolean isUnset(String key) {
+		return (getState(key) == State.UNSET);
+	}
+
+	/**
+	 * @param key
+	 * @return the attribute value (may be <code>null</code>)
+	 */
+	public String getValue(String key) {
+		Attribute a = map.get(key);
+		return a != null ? a.getValue() : null;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder buf = new StringBuilder();
+		buf.append(getClass().getSimpleName());
+		buf.append("["); //$NON-NLS-1$
+		buf.append(" "); //$NON-NLS-1$
+		for (Attribute a : map.values()) {
+			buf.append(a.toString());
+			buf.append(" "); //$NON-NLS-1$
+		}
+		buf.append("]"); //$NON-NLS-1$
+		return buf.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		return map.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Attributes))
+			return false;
+		Attributes other = (Attributes) obj;
+		return this.map.equals(other.map);
+	}
+
+}

org.eclipse.jgit/src/org/eclipse/jgit/attributes/AttributesNode.java
@@ -50,7 +50,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.ListIterator;
-import java.util.Map;
 
 import org.eclipse.jgit.lib.Constants;
 
@@ -134,11 +133,12 @@ public class AttributesNode {
 	 *            true if the target item is a directory.
 	 * @param attributes
 	 *            Map that will hold the attributes matching this entry path. If
-	 *            it is not empty, this method will NOT override any
-	 *            existing entry.
+	 *            it is not empty, this method will NOT override any existing
+	 *            entry.
+	 * @since 4.2
 	 */
-	public void getAttributes(String entryPath, boolean isDirectory,
-			Map<String, Attribute> attributes) {
+	public void getAttributes(String entryPath,
+			boolean isDirectory, Attributes attributes) {
 		// Parse rules in the reverse order that they were read since the last
 		// entry should be used
 		ListIterator<AttributesRule> ruleIterator = rules.listIterator(rules
@@ -153,7 +153,7 @@ public class AttributesNode {
 				while (attributeIte.hasPrevious()) {
 					Attribute attr = attributeIte.previous();
 					if (!attributes.containsKey(attr.getKey()))
-						attributes.put(attr.getKey(), attr);
+						attributes.putAttribute(attr);
 				}
 			}
 		}

ShoOgino/jgitFile

org.eclipse.jgit.test/tst/org/eclipse/jgit/attributes/AttributesNodeDirCacheIteratorTest.java
@@ -52,9 +52,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.attributes.Attribute.State;
@@ -258,12 +256,14 @@ public class AttributesNodeDirCacheIteratorTest extends RepositoryTestCase {
 			assertTrue(nodeAttrs == null || nodeAttrs.isEmpty());
 		else {
 
-			Map<String, Attribute> entryAttributes = new LinkedHashMap<String, Attribute>();
-			attributesNode.getAttributes(pathName, false, entryAttributes);
+			Attributes entryAttributes = new Attributes();
+			attributesNode.getAttributes(pathName,
+					false, entryAttributes);
 
 			if (nodeAttrs != null && !nodeAttrs.isEmpty()) {
 				for (Attribute attribute : nodeAttrs) {
-					assertThat(entryAttributes.values(), hasItem(attribute));
+					assertThat(entryAttributes.getAll(),
+							hasItem(attribute));
 				}
 			} else {
 				assertTrue(

org.eclipse.jgit.test/tst/org/eclipse/jgit/attributes/AttributesNodeWorkingTreeIteratorTest.java
@@ -53,9 +53,7 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.eclipse.jgit.attributes.Attribute.State;
 import org.eclipse.jgit.errors.CorruptObjectException;
@@ -227,12 +225,14 @@ public class AttributesNodeWorkingTreeIteratorTest extends RepositoryTestCase {
 			assertTrue(nodeAttrs == null || nodeAttrs.isEmpty());
 		else {
 
-			Map<String, Attribute> entryAttributes = new LinkedHashMap<String, Attribute>();
-			attributesNode.getAttributes(pathName, false, entryAttributes);
+			Attributes entryAttributes = new Attributes();
+			attributesNode.getAttributes(pathName,
+					false, entryAttributes);
 
 			if (nodeAttrs != null && !nodeAttrs.isEmpty()) {
 				for (Attribute attribute : nodeAttrs) {
-					assertThat(entryAttributes.values(), hasItem(attribute));
+					assertThat(entryAttributes.getAll(),
+							hasItem(attribute));
 				}
 			} else {
 				assertTrue(

org.eclipse.jgit.test/tst/org/eclipse/jgit/attributes/TreeWalkAttributeTest.java
@@ -802,8 +802,9 @@ public class TreeWalkAttributeTest extends RepositoryTestCase {
 		assertEquals(type, walk.getFileMode(0));
 
 		assertEquals(checkinAttributes,
-				asSet(ci_walk.getAttributes().values()));
-		assertEquals(checkoutAttributes, asSet(walk.getAttributes().values()));
+				asSet(ci_walk.getAttributes().getAll()));
+		assertEquals(checkoutAttributes,
+				asSet(walk.getAttributes().getAll()));
 
 		if (D.equals(type)) {
 			walk.enterSubtree();

org.eclipse.jgit/src/org/eclipse/jgit/attributes/Attributes.java
@@ -1,0 +1,202 @@
+/*
+ * Copyright (C) 2015, Ivan Motsch <ivan.motsch@bsiag.com>
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.attributes;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.eclipse.jgit.attributes.Attribute.State;
+
+/**
+ * Represents a set of attributes for a path
+ * <p>
+ *
+ * @since 4.2
+ */
+public final class Attributes {
+	private final Map<String, Attribute> map = new LinkedHashMap<>();
+
+	/**
+	 * Creates a new instance
+	 *
+	 * @param attributes
+	 */
+	public Attributes(Attribute... attributes) {
+		if (attributes != null) {
+			for (Attribute a : attributes) {
+				put(a);
+			}
+		}
+	}
+
+	/**
+	 * @return true if the set does not contain any attributes
+	 */
+	public boolean isEmpty() {
+		return map.isEmpty();
+	}
+
+	/**
+	 * @param key
+	 * @return the attribute or null
+	 */
+	public Attribute get(String key) {
+		return map.get(key);
+	}
+
+	/**
+	 * @return all attributes
+	 */
+	public Collection<Attribute> getAll() {
+		return new ArrayList<>(map.values());
+	}
+
+	/**
+	 * @param a
+	 */
+	public void put(Attribute a) {
+		map.put(a.getKey(), a);
+	}
+
+	/**
+	 * @param key
+	 */
+	public void remove(String key) {
+		map.remove(key);
+	}
+
+	/**
+	 * @param key
+	 * @return true if the {@link Attributes} contains this key
+	 */
+	public boolean containsKey(String key) {
+		return map.containsKey(key);
+	}
+
+	/**
+	 * Returns the state.
+	 *
+	 * @param key
+	 *
+	 * @return the state (never returns <code>null</code>)
+	 */
+	public Attribute.State getState(String key) {
+		Attribute a = map.get(key);
+		return a != null ? a.getState() : Attribute.State.UNSPECIFIED;
+	}
+
+	/**
+	 * @param key
+	 * @return true if the key is {@link State#SET}, false in all other cases
+	 */
+	public boolean isSet(String key) {
+		return (getState(key) == State.SET);
+	}
+
+	/**
+	 * @param key
+	 * @return true if the key is {@link State#UNSET}, false in all other cases
+	 */
+	public boolean isUnset(String key) {
+		return (getState(key) == State.UNSET);
+	}
+
+	/**
+	 * @param key
+	 * @return true if the key is {@link State#UNSPECIFIED}, false in all other
+	 *         cases
+	 */
+	public boolean isUnspecified(String key) {
+		return (getState(key) == State.UNSPECIFIED);
+	}
+
+	/**
+	 * @param key
+	 * @return true if the key is {@link State#CUSTOM}, false in all other cases
+	 *         see {@link #getValue(String)} for the value of the key
+	 */
+	public boolean isCustom(String key) {
+		return (getState(key) == State.CUSTOM);
+	}
+
+	/**
+	 * @param key
+	 * @return the attribute value (may be <code>null</code>)
+	 */
+	public String getValue(String key) {
+		Attribute a = map.get(key);
+		return a != null ? a.getValue() : null;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder buf = new StringBuilder();
+		buf.append(getClass().getSimpleName());
+		buf.append("["); //$NON-NLS-1$
+		buf.append(" "); //$NON-NLS-1$
+		for (Attribute a : map.values()) {
+			buf.append(a.toString());
+			buf.append(" "); //$NON-NLS-1$
+		}
+		buf.append("]"); //$NON-NLS-1$
+		return buf.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		return map.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Attributes))
+			return false;
+		Attributes other = (Attributes) obj;
+		return this.map.equals(other.map);
+	}
+
+}

org.eclipse.jgit/src/org/eclipse/jgit/attributes/AttributesNode.java
@@ -50,7 +50,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.ListIterator;
-import java.util.Map;
 
 import org.eclipse.jgit.lib.Constants;
 
@@ -134,11 +133,12 @@ public class AttributesNode {
 	 *            true if the target item is a directory.
 	 * @param attributes
 	 *            Map that will hold the attributes matching this entry path. If
-	 *            it is not empty, this method will NOT override any
-	 *            existing entry.
+	 *            it is not empty, this method will NOT override any existing
+	 *            entry.
+	 * @since 4.2
 	 */
-	public void getAttributes(String entryPath, boolean isDirectory,
-			Map<String, Attribute> attributes) {
+	public void getAttributes(String entryPath,
+			boolean isDirectory, Attributes attributes) {
 		// Parse rules in the reverse order that they were read since the last
 		// entry should be used
 		ListIterator<AttributesRule> ruleIterator = rules.listIterator(rules
@@ -153,7 +153,7 @@ public class AttributesNode {
 				while (attributeIte.hasPrevious()) {
 					Attribute attr = attributeIte.previous();
 					if (!attributes.containsKey(attr.getKey()))
-						attributes.put(attr.getKey(), attr);
+						attributes.put(attr);
 				}
 			}
 		}