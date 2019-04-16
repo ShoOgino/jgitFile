eclipse-jgit/jgit

tree:<depth>: do not revisit tree during packing
If a tree is visited during pack and filtered out with tree:<depth>, we
may need to include it if it is visited again at a lower depth.

Until now we revisit it no matter what the depth is. Now, avoid
visiting it if it has been visited at a lower or equal depth.

Change-Id: I68cc1d08f1999a8336684a05fe16e7ae51898866
Signed-off-by: Matthew DeVore <matvore@gmail.com>

org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/pack/PackWriter.java
@@ -63,6 +63,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -873,19 +874,34 @@ public class PackWriter implements AutoCloseable {
 	}
 
 	/**
-	 * A seen policy which causes objects to be visited repeatedly by making
-	 * {@code checkSeen} always return {@code false}.
+	 * A seen policy which uses the depth at which the object is seen to decide
+	 * if re-traversal is necessary. In particular, if the object has already
+	 * been seen at this depth or shallower, it is not necessary to re-visit at
+	 * this depth.
 	 */
-	private static final ObjectWalk.SeenPolicy ALWAYS_VISIT_SEEN_POLICY =
-			new ObjectWalk.SeenPolicy() {
-		@Override
-		public boolean checkSeen(RevObject o) {
-			return false;
+	private class DepthAwareSeenPolicy implements ObjectWalk.SeenPolicy {
+		private final Map<ObjectId, Long> lowestDepthSeen = new HashMap<>();
+
+		private final ObjectWalk walk;
+
+		DepthAwareSeenPolicy(ObjectWalk walk) {
+			this.walk = requireNonNull(walk);
 		}
 
 		@Override
-		public void setSeen(RevObject o) {}
-	};
+		public boolean checkSeen(RevObject o) {
+			Long lastDepth = lowestDepthSeen.get(o);
+			if (lastDepth == null) {
+				return false;
+			}
+			return walk.getTreeDepth() >= lastDepth;
+		}
+
+		@Override
+		public void setSeen(RevObject o) {
+			lowestDepthSeen.put(o, (long) walk.getTreeDepth());
+		}
+	}
 
 	/**
 	 * Prepare the list of objects to be written to the pack stream.
@@ -929,7 +945,7 @@ public class PackWriter implements AutoCloseable {
 			throw new IllegalArgumentException(
 					JGitText.get().shallowPacksRequireDepthWalk);
 		if (filterSpec.getTreeDepthLimit() >= 0) {
-			walk.setSeenPolicy(ALWAYS_VISIT_SEEN_POLICY);
+			walk.setSeenPolicy(new DepthAwareSeenPolicy(walk));
 		}
 		findObjectsToPack(countingMonitor, walk, interestingObjects,
 				uninterestingObjects, noBitmaps);

ShoOgino/jgitFile

org.eclipse.jgit/src/org/eclipse/jgit/internal/storage/pack/PackWriter.java
@@ -63,6 +63,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -873,19 +874,35 @@ public class PackWriter implements AutoCloseable {
 	}
 
 	/**
-	 * A visitation policy which causes objects to be visited repeatedly by
-	 * making {@code shouldVisit} always return {@code true}.
+	 * A visitation policy which uses the depth at which the object is seen to
+	 * decide if re-traversal is necessary. In particular, if the object has
+	 * already been visited at this depth or shallower, it is not necessary to
+	 * re-visit at this depth.
 	 */
-	private static final ObjectWalk.VisitationPolicy ALWAYS_VISIT_POLICY =
-			new ObjectWalk.VisitationPolicy() {
-		@Override
-		public boolean shouldVisit(RevObject o) {
-			return true;
+	private class DepthAwareVisitationPolicy
+			implements ObjectWalk.VisitationPolicy {
+		private final Map<ObjectId, Long> lowestDepthVisited = new HashMap<>();
+
+		private final ObjectWalk walk;
+
+		DepthAwareVisitationPolicy(ObjectWalk walk) {
+			this.walk = requireNonNull(walk);
 		}
 
 		@Override
-		public void visited(RevObject o) {}
-	};
+		public boolean shouldVisit(RevObject o) {
+			Long lastDepth = lowestDepthVisited.get(o);
+			if (lastDepth == null) {
+				return true;
+			}
+			return walk.getTreeDepth() < lastDepth;
+		}
+
+		@Override
+		public void visited(RevObject o) {
+			lowestDepthVisited.put(o, (long) walk.getTreeDepth());
+		}
+	}
 
 	/**
 	 * Prepare the list of objects to be written to the pack stream.
@@ -929,7 +946,7 @@ public class PackWriter implements AutoCloseable {
 			throw new IllegalArgumentException(
 					JGitText.get().shallowPacksRequireDepthWalk);
 		if (filterSpec.getTreeDepthLimit() >= 0) {
-			walk.setVisitationPolicy(ALWAYS_VISIT_POLICY);
+			walk.setVisitationPolicy(new DepthAwareVisitationPolicy(walk));
 		}
 		findObjectsToPack(countingMonitor, walk, interestingObjects,
 				uninterestingObjects, noBitmaps);