eclipse-jgit/jgit

Fix human name for local .bundle files
Bug: 560903
Change-Id: I15d45330398cc573940265d16a2db29ddce085aa
Signed-off-by: Konrad Windszus <konrad_w@gmx.de>

org.eclipse.jgit/src/org/eclipse/jgit/lib/Constants.java
@@ -385,6 +385,13 @@ public final class Constants {
 	/** A bare repository typically ends with this string */
 	public static final String DOT_GIT_EXT = ".git";
 
+	/** 
+	 * The default extension for local bundle files 
+	 * 
+	 * @since 5.8
+	 */
+	public static final String DOT_BUNDE_EXT = ".bundle";
+
 	/**
 	 * Name of the attributes file
 	 *

org.eclipse.jgit/src/org/eclipse/jgit/transport/URIish.java
@@ -738,6 +738,11 @@ public class URIish implements Serializable {
 		else if (result.endsWith(Constants.DOT_GIT_EXT))
 			result = result.substring(0, result.length()
 					- Constants.DOT_GIT_EXT.length());
+		if (("file".equals(scheme) || LOCAL_FILE.matcher(s).matches())
+				&& result.endsWith(Constants.DOT_BUNDE_EXT)) {
+			result = result.substring(0,
+					result.length() - Constants.DOT_BUNDE_EXT.length());
+		}
 		return result;
 	}
 

ShoOgino/jgitFile

org.eclipse.jgit/src/org/eclipse/jgit/lib/Constants.java
@@ -386,6 +386,13 @@ public final class Constants {
 	public static final String DOT_GIT_EXT = ".git";
 
 	/**
+	 * The default extension for local bundle files
+	 *
+	 * @since 5.8
+	 */
+	public static final String DOT_BUNDLE_EXT = ".bundle";
+
+	/**
 	 * Name of the attributes file
 	 *
 	 * @since 3.7

org.eclipse.jgit/src/org/eclipse/jgit/transport/URIish.java
@@ -738,6 +738,11 @@ public class URIish implements Serializable {
 		else if (result.endsWith(Constants.DOT_GIT_EXT))
 			result = result.substring(0, result.length()
 					- Constants.DOT_GIT_EXT.length());
+		if (("file".equals(scheme) || LOCAL_FILE.matcher(s).matches())
+				&& result.endsWith(Constants.DOT_BUNDLE_EXT)) {
+			result = result.substring(0,
+					result.length() - Constants.DOT_BUNDLE_EXT.length());
+		}
 		return result;
 	}
 